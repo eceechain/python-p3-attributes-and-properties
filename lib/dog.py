3291e56ffe76f2f537b0fb63ec912dcd75df80b1
ecee <you@example.com>
2023-11-26 13:33:34 +0300

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,30 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name, breed=None):
+        self._breed = None
+        self._name = None  # Added _name attribute
+        self.name = name
+
+    def set_name(self, value):  # Added set_name method
+        if isinstance(value, str) and 0 < len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_name(self):  # Added get_name method
+        return self._name
+
+    name = property(get_name, set_name)  # Using property() to define name property
+
+    def get_breed(self):  # Added get_breed method
+        return self._breed
+
+    def set_breed(self, value):  # Added set_breed method
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)  # Using property() to define breed property
+    
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,31 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name, job=None):
+        self._name = None
+        self._job = None
+        self.set_name(name)
+        self.set_job(job)
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if isinstance(value, str) and 0 < len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in the list of approved jobs.")
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+    
+
